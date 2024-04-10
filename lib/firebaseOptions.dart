7888bff389dd0dfb533b63867855fab24f5af123
Maulik Patel <139054994+Maulik2042@users.noreply.github.com>
2024-04-10 12:34:51 +0530

    Delete lib/firebaseOptions.dart
    firebaseOptions.dart file contains your firebase "apikey" and 'appId' and 'messageID' etc

--- lib/firebaseOptions.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-
-FirebaseOptions firebaseOptions = const FirebaseOptions(
-    apiKey: 'AIzaSyBk0KzrTnb3Po0qJaH8VWzlVHqfCO6l8Qo',
-    appId: '1:587554598232:android:bee97618dd1a313063092d',
-    messagingSenderId: '587554598232',
-    projectId: 'e-commerce-4e756');
